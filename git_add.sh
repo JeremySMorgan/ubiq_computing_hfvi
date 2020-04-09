7ff0ef72a796da98d6d95447b0a18593e3142958
jeremy.morgan <jsmorgan6@gmail.com>
2020-04-09 06:30:37 -0700

    ported project to CMake

---
+++ git_add.sh
@@ -0,0 +1,12 @@
+
+git add run\ script\ command
+
+git add CMakeLists.txt
+git add *.cpp
+git add include\*.hpp
+git add third_party\*.h
+
+git add arduino_code/*.ino
+
+git add git_add.sh
+git add .gitignore
--- main.cpp
+++ main.cpp
@@ -3,34 +3,53 @@
 
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <iostream>             // for cout
+#include <opencv2/opencv.hpp>   // Include OpenCV API
+#include "include/example.hpp"          // Include a short list of convenience functions for rendering
 
 // Hello RealSense example demonstrates the basics of connecting to a RealSense device
 // and taking advantage of depth data
-int main(int argc, char * argv[]) try
-{
+int main(int argc, char * argv[]) try{
+
+
+    window app(1280, 720, "RealSense Capture Example");
+
+    // Declare depth colorizer for pretty visualization of depth data
+    rs2::colorizer color_map;
+
+    // Declare rates printer for showing streaming rates of the enabled streams.
+    rs2::rates_printer printer;
+
     // Create a Pipeline - this serves as a top-level API for streaming and processing frames
-    rs2::pipeline p;
+    rs2::pipeline pipe;
 
     // Configure and start the pipeline
-    p.start();
+    pipe.start();
 
-    while (true)
-    {
+    while (app){
+
+        rs2::frameset data = pipe.wait_for_frames().
+                                apply_filter(printer).
+                                apply_filter(color_map);    // colorize the depth data
+
+        app.show(data);
+
+        auto mat = frame_to_mat();
+
         // Block program until frames arrive
-        rs2::frameset frames = p.wait_for_frames();
-
-        // Try to get a frame of a depth image
-        rs2::depth_frame depth = frames.get_depth_frame();
-
-        // Get the depth frame's dimensions
-        float width = depth.get_width();
-        float height = depth.get_height();
-
-        // Query the distance from the camera to the object in the center of the image
-        float dist_to_center = depth.get_distance(width / 2, height / 2);
-
-        // Print the distance
-        std::cout << "The camera is facing an object " << dist_to_center << " meters away \r";
+//        rs2::frameset frames = p.wait_for_frames();
+//
+//        // Try to get a frame of a depth image
+//        rs2::depth_frame depth = frames.get_depth_frame();
+//
+//        // Get the depth frame's dimensions
+//        float width = depth.get_width();
+//        float height = depth.get_height();
+//
+//        // Query the distance from the camera to the object in the center of the image
+//        float dist_to_center = depth.get_distance(width / 2, height / 2);
+//
+//        // Print the distance
+//        std::cout << "The camera is facing an object " << dist_to_center << " meters away \r";
     }
 
     return EXIT_SUCCESS;
